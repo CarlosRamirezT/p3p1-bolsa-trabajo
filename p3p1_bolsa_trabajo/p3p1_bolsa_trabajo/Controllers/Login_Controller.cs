26e35b14f783970edd309b686cd19aa9f7878838
masterlynk <roberlynrijo@gmail.com>
2020-01-29 08:36:06 +0400

    Creacion de el login y signup

---
+++ p3p1_bolsa_trabajo/p3p1_bolsa_trabajo/Controllers/Login_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using p3p1_bolsa_trabajo.Models;
+
+namespace p3p1_bolsa_trabajo.Controllers
+{
+    public class Login_Controller : Controller
+    {
+        // GET: Login_
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(p3p1_bolsa_trabajo.Models.Usuario UserModel)
+        {
+            using (Model_Usuarios db = new Model_Usuarios())
+            {
+                var userDetails = db.Usuarios.Where(x => x.email == UserModel.email && x.password_usuario == UserModel.password_usuario).FirstOrDefault();
+                if(userDetails == null)
+                {
+                    UserModel.LoginErrorMessage = "email o password invalida.";
+                    return View("Login", UserModel);
+                }
+                else
+                {
+                    Session["id_usuarios"] = userDetails.id_usuarios;
+                    return RedirectToAction("Index", "Ofertas_Categorias_");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ p3p1_bolsa_trabajo/p3p1_bolsa_trabajo/Controllers/User_Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using p3p1_bolsa_trabajo.Models;
+
+namespace p3p1_bolsa_trabajo.Controllers
+{
+    public class User_Controller : Controller
+    {
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Usuario usuarioModel = new Usuario();
+            return View(usuarioModel);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(Usuario usuarioModel)
+        {
+            using (Model_Usuarios dbmodel = new Model_Usuarios())
+            {
+                if (dbmodel.Usuarios.Any(x => x.email == usuarioModel.email))
+                {
+                    ViewBag.DuplicateMessage = "El email ingresado esta en uso";
+                    return View("AddOrEdit", usuarioModel);
+                }
+                dbmodel.Usuarios.Add(usuarioModel);
+                dbmodel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccesMessage = "Registro Exitoso";
+            return View("AddOrEdit", new Usuario());
+        }
+    }
+}
