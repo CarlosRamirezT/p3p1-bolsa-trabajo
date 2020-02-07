454f110cf4a98ad85c9ba41721faaefd5be22b84
Cadara <carlosdanielrt@gmail.com>
2020-02-07 05:06:40 -0400

    [ADD] controller for categories [IMP] admin can create, edit, view details and delete categories [MOD] categories tab in admin navbar redirects to categories index

---
+++ p3p1-bolsa-empleo-new/p3p1_bolsa_trabajo_new/Controllers/CategoriasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using p3p1_bolsa_trabajo_new.Models;
+
+namespace p3p1_bolsa_trabajo_new.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private p3p1BolsaTrabajoEntitiesCategorias db = new p3p1BolsaTrabajoEntitiesCategorias();
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(db.categoriaOfertaEmpleos.ToList());
+        }
+
+        // GET: Categorias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categoriaOfertaEmpleo categoriaOfertaEmpleo = db.categoriaOfertaEmpleos.Find(id);
+            if (categoriaOfertaEmpleo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoriaOfertaEmpleo);
+        }
+
+        // GET: Categorias/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categorias/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_categoria_ofertas,titulo")] categoriaOfertaEmpleo categoriaOfertaEmpleo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.categoriaOfertaEmpleos.Add(categoriaOfertaEmpleo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(categoriaOfertaEmpleo);
+        }
+
+        // GET: Categorias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categoriaOfertaEmpleo categoriaOfertaEmpleo = db.categoriaOfertaEmpleos.Find(id);
+            if (categoriaOfertaEmpleo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoriaOfertaEmpleo);
+        }
+
+        // POST: Categorias/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_categoria_ofertas,titulo")] categoriaOfertaEmpleo categoriaOfertaEmpleo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(categoriaOfertaEmpleo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoriaOfertaEmpleo);
+        }
+
+        // GET: Categorias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categoriaOfertaEmpleo categoriaOfertaEmpleo = db.categoriaOfertaEmpleos.Find(id);
+            if (categoriaOfertaEmpleo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoriaOfertaEmpleo);
+        }
+
+        // POST: Categorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            categoriaOfertaEmpleo categoriaOfertaEmpleo = db.categoriaOfertaEmpleos.Find(id);
+            db.categoriaOfertaEmpleos.Remove(categoriaOfertaEmpleo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ p3p1-bolsa-empleo-new/p3p1_bolsa_trabajo_new/Controllers/CategoriasOnlyController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using p3p1_bolsa_trabajo_new.Models;
+
+namespace p3p1_bolsa_trabajo_new.Controllers
+{
+    public class CategoriasOnlyController : Controller
+    {
+        private p3p1BolsaTrabajoEntitiesCategoriaOfertas db = new p3p1BolsaTrabajoEntitiesCategoriaOfertas();
+
+        // GET: CategoriasOnly
+        public ActionResult Index()
+        {
+            if (Session["id_usuarios"] != null)
+            {
+                return View(db.categoriaOfertaEmpleos.ToList());
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please Login";
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        // GET: CategoriasOnly/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (Session["id_usuarios"] != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                categoriaOfertaEmpleo categoriaOfertaEmpleo = db.categoriaOfertaEmpleos.Find(id);
+                if (categoriaOfertaEmpleo == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(categoriaOfertaEmpleo);
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please Login";
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        // GET: CategoriasOnly/Create
+        public ActionResult Create()
+        {
+            if (Session["id_usuarios"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please Login";
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        // POST: CategoriasOnly/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_categoria_ofertas,titulo")] categoriaOfertaEmpleo categoriaOfertaEmpleo)
+        {
+            if (Session["id_usuarios"] != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    db.categoriaOfertaEmpleos.Add(categoriaOfertaEmpleo);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return View(categoriaOfertaEmpleo);
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please Login";
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        // GET: CategoriasOnly/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (Session["id_usuarios"] != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                categoriaOfertaEmpleo categoriaOfertaEmpleo = db.categoriaOfertaEmpleos.Find(id);
+                if (categoriaOfertaEmpleo == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(categoriaOfertaEmpleo);
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please Login";
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        // POST: CategoriasOnly/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_categoria_ofertas,titulo")] categoriaOfertaEmpleo categoriaOfertaEmpleo)
+        {
+            if (Session["id_usuarios"] != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(categoriaOfertaEmpleo).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(categoriaOfertaEmpleo);
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please Login";
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        // GET: CategoriasOnly/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (Session["id_usuarios"] != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                categoriaOfertaEmpleo categoriaOfertaEmpleo = db.categoriaOfertaEmpleos.Find(id);
+                if (categoriaOfertaEmpleo == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(categoriaOfertaEmpleo);
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please Login";
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        // POST: CategoriasOnly/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            if (Session["id_usuarios"] != null)
+            {
+                categoriaOfertaEmpleo categoriaOfertaEmpleo = db.categoriaOfertaEmpleos.Find(id);
+                db.categoriaOfertaEmpleos.Remove(categoriaOfertaEmpleo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Please Login";
+                return RedirectToAction("Login", "Login");
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
